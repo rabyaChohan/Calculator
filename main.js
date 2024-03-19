f65cf4061b6f62d0e5b118fcbbce64d0a12f6d21
“[firstname <rabya759@gmail.com>
2024-03-20 01:24:13 +0500

    Update

---
+++ main.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter First Number", type: "number", name: "secondNumber" },
+    { message: "Select Your Operation",
+        type: "list",
+        name: "operation",
+        choices: ["Addition", "Subtration", "Multiplication", "Division"], }
+]);
+if (answers.operation === "Addition") {
+    console.log(chalk.blue(answers.firstNumber + answers.secondNumber));
+}
+else if (answers.operation === "Subtration") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operation === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operation === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Please Select valid Operator");
+}
